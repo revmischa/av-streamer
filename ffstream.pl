963709cddb608cfdce6d445e23510a2414ee675b
Mischa S <revmischa@cpan.org>
2011-07-09 19:58:27 -0700

    syntax

--- ffstream.pl
+++ ffstream.pl
@@ -16,7 +16,7 @@ my $s = AV::Streamer::App::Streamer->new_with_options;
 # get output uri
 unless ($s->output_uri) {
     my $extra = $s->extra_argv;
-    die "No output file specified\n" $extra && $extra->[0];
+    die "No output file specified\n" unless $extra && $extra->[0];
     $s->output_uri($extra->[0]);
 }
 
